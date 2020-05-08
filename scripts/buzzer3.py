06b9a80c3b3c35d86616eddc33c53ed3537dd609
Takumi Kobayashi <git.origami@gmail.com>
2020-05-08 06:04:22 +0000

    Modify file:scripts/buzzer3.bash

--- scripts/buzzer3.py
+++ scripts/buzzer3.py
@@ -3,7 +3,7 @@ import rospy
 from std_msgs.msg import UInt16
 
 def write_freq(hz=0):
-    bfile = "/drv/rtbuzzer0"
+    bfile = "/dev/rtbuzzer0"
     try:
 	with open(bfile,"w") as f:
 	    f.write(str(hz)+"\n")
@@ -11,7 +11,7 @@ def write_freq(hz=0):
 	rospy.logerr("can't write to " + bfile)
 
 def recv_buzzer(data):
-    write_fleq(data.data)
+    write_freq(data.data)
 
 if __name__ == '__main__':
     rospy.init_node('buzzer')
